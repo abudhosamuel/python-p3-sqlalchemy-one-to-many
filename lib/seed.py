f06c4a04688b0c2ce7fbde90ca084ee9b270fb48
abudhosamuel <abudhosamuel@gmail.com>
2024-09-11 12:43:58 +0300

    one to many

--- lib/seed.py
+++ lib/seed.py
@@ -1,55 +1,38 @@
-#!/usr/bin/env python3
-
-from faker import Faker
-import random
-
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import ForeignKey, Column, Integer, String
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
 
-from models import Game, Review
+engine = create_engine('sqlite:///one_to_many.db')
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///one_to_many.db')
-    Session = sessionmaker(bind=engine)
-    session = Session()
+Base = declarative_base()
 
-    session.query(Game).delete()
-    session.query(Review).delete()
+class Game(Base):
+    __tablename__ = 'games'
 
-    fake = Faker()
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    genre = Column(String())
+    platform = Column(String())
+    price = Column(Integer())
 
-    genres = ['action', 'adventure', 'strategy',
-        'puzzle', 'first-person shooter', 'racing']
-    platforms = ['nintendo 64', 'gamecube', 'wii', 'wii u', 'switch',
-        'playstation', 'playstation 2', 'playstation 3', 'playstation 4',
-        'playstation 5', 'xbox', 'xbox 360', 'xbox one', 'pc']
+    reviews = relationship('Review', backref=backref('game'))
 
-    games = []
-    for i in range(50):
-        game = Game(
-            title=fake.unique.name(),
-            genre=random.choice(genres),
-            platform=random.choice(platforms),
-            price=random.randint(5, 60)
-        )
+    def __repr__(self):
+        return f'Game(id={self.id}, ' + \
+            f'title={self.title}, ' + \
+            f'platform={self.platform})'
 
-        # add and commit individually to get IDs back
-        session.add(game)
-        session.commit()
+class Review(Base):
+    __tablename__ = 'reviews'
 
-        games.append(game)
+    id = Column(Integer(), primary_key=True)
+    score = Column(Integer())
+    comment = Column(String())
 
-    reviews = []
-    for game in games:
-        for i in range(random.randint(1,5)):
-            review = Review(
-                score=random.randint(0, 10),
-                comment=fake.sentence(),
-                game_id=game.id
-            )
+    game_id = Column(Integer(), ForeignKey('games.id'))
 
-            reviews.append(review)
-    
-    session.bulk_save_objects(reviews)
-    session.commit()
-    session.close()
+    def __repr__(self):
+        return f'Review(id={self.id}, ' + \
+            f'score={self.score}, ' + \
+            f'game_id={self.game_id})'
